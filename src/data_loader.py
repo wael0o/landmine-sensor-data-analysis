b6998d1ef1f19c5d6e710d9a3f5a218f8b0ca22d
wael0o <waelbelhadj393@gmail.com>
2026-02-25 13:55:59 +0100

    Delete src directory

--- src/data_loader.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def load_data(path="data/Mine_Dataset.xls"):
-    df = pd.read_excel(path, sheet_name="Normalized_Data")
-    X = df[["V","H","S"]].values
-    y = df["M"].values
-    return X, y, df
